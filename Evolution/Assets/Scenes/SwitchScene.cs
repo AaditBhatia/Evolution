3c260407eac95acaeef36cd8700c777f6e4689f3
AaditBhatia <aadit.bhatia93@gmail.com>
2022-12-04 21:37:44 -0500

    Testing Github

--- Evolution/Assets/Scenes/SwitchScene.cs
+++ Evolution/Assets/Scenes/SwitchScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
@@ -11,8 +12,5 @@ public class SwitchScene : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
